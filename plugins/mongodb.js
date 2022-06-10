f7acd35581f219cb038a0ac26f5441cb50f3a6aa
Kelly <68624696+yrkimyy@users.noreply.github.com>
2022-06-10 14:00:13 +0900

    extract mongo

--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -1,8 +1,8 @@
 const fp = require('fastify-plugin')
 
 module.exports = fp(async function (fastify, opts) {
-  fastify.register(require('@fastify/mongodb'), {
-    forceClose: true,
-    url: 'mongodb://hello:world@localhost:27017/baedal'
-    })
+  // fastify.register(require('@fastify/mongodb'), {
+  //   forceClose: true,
+  //   url: 'mongodb://hello:world@localhost:27017/baedal'
+  //   })
 })
