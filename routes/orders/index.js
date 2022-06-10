6c0cf4f97eea11f936cd5ae64e18464105a9ba1f
Kelly <68624696+yrkimyy@users.noreply.github.com>
2022-06-10 13:55:59 +0900

    extract mongodb

--- routes/orders/index.js
+++ routes/orders/index.js
@@ -2,8 +2,8 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    const orders = this.mongo.db.collection('order')
-    const result = orders.find({}).toArray()
-    reply.send(result)
+    // const orders = this.mongo.db.collection('order')
+    // const result = orders.find({}).toArray()
+    reply.send("yes")
   })
 }
